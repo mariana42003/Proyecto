0803acb1cbca770e81003665900c09e813d3559e
Manuel <manuelesteban13vigon@gmail.com>
2026-06-29 20:32:00 -0500

    feat ui: Subo vista de clientes corregida con barra de carrito y vista de pedidos

--- Trimestre 3/Maquetacion/Javascript/funciones-inicio.js
+++ Trimestre 3/Maquetacion/Javascript/funciones-inicio.js
@@ -22,3 +22,29 @@ $('#carrusel').on('click', '.agregar-b', function() {
 
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    // Capturamos los elementos del DOM
+    const btnCarrito = document.getElementById("btnCarrito");
+    const btnCerrarCarrito = document.getElementById("btnCerrarCarrito");
+    const carritoSidebar = document.getElementById("carritoSidebar");
+
+    // Función para abrir el carrito
+    function abrirCarrito(e) {
+        e.preventDefault(); // Evita que el enlace '#' recargue o mueva la página
+        carritoSidebar.classList.add("active");
+
+    }
+
+    // Función para cerrar el carrito
+    function cerrarCarrito() {
+        carritoSidebar.classList.remove("active");
+
+    }
+
+    // Eventos de escucha
+    btnCarrito.addEventListener("click", abrirCarrito);
+    btnCerrarCarrito.addEventListener("click", cerrarCarrito);
+});
+
+
+
